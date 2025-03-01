7233337c800dd5114a0d9348f223a6c126aac6aa
trishi880988 <gauravyt8803@gmail.com>
2025-03-01 08:48:26 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -74,8 +74,8 @@ async def token_handler(client, message):
     user_id = message.chat.id
     if len(message.command) <= 1:
         image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/skillwithgaurav")
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/ytbr_67")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
