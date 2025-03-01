5a933aa2e5df89b8b94ebca58556f15b6352278b
trishi880988 <gauravyt8803@gmail.com>
2025-03-01 10:52:29 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,7 +73,7 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
+        image_url = "https://i.imghippo.com/files/Bdr9783aww.jpg"
         join_button = InlineKeyboardButton("Join Channel", url="https://t.me/skillwithgaurav")
         premium = InlineKeyboardButton("Get Premium", url="https://t.me/ytbr_67")   
         keyboard = InlineKeyboardMarkup([
